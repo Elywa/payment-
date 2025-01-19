dd3d2cf801bf5c209cb4c248de7ec61f9f01c20b
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2025-01-19 13:34:04 +0200

    add styles

--- lib/main.dart
+++ lib/main.dart
@@ -39,14 +39,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -101,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           //
           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
+          // action in the IDE, or press "p" in the console), to see thel
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
